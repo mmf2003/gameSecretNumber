ba4a37fe5586aa4c5bff6f5b63944201167962ef
Chris_auty <mmf2003@ukr.net>
2024-01-11 20:37:19 +0200

    Modified game function and some tests done

--- scrypt.js
+++ scrypt.js
@@ -12,21 +12,25 @@ function numberOfPlayers(players, attemps, range) {
     }
 }
 
-//need to add range of secret numbers
-
 function game(players, attemps, range, secret) {
     numberOfPlayers(players,attemps, range); 
 
     for (let i = 0; i < playersInGame.length; i++) {
         let attemp;
+        console.log(`Secret number is ${secret}\nAttempt ${i + 1}:`);
         for (let k = 0; k < playersInGame[i].length; k++) {
+            
             attemp = playersInGame[i][k];
             if (secret == playersInGame[i][k]) {
-                console.log(`Player${k+1} wins on ${i+1} attempt`);
+                console.log(`Player${k+1} suggested that secret number is ${playersInGame[i][k]} and won on ${i+1} attempt`);
                 break;
+            }
+            else {
+                console.log(`player${k + 1} suggested that secret number is ${playersInGame[i][k]} and lost`);
             }
         }
         if (attemp == secret) break;
     }
 }
 
+game(10,100,1000,10); //tests
