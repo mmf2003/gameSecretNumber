cb6d7c80bc3e70056cc2da0628245f15118130f7
Chris_auty <mmf2003@ukr.net>
2024-01-11 19:32:27 +0200

    Modified numberOfPlayers function

--- scrypt.js
+++ scrypt.js
@@ -3,15 +3,19 @@
 let playersInGame = [];
 
 function numberOfPlayers(players, attemps) {
-    for (let i = 0; i < players; i++) {
-        let playerX = [];
-        for (let i = 0; i < attemps; i++) {
-            playerX.push((Math.floor(Math.random() * 6)));
+    for (let i = 0; i < attemps; i++) {
+        let attemptX = [];
+        for (let i = 0; i < players; i++) {
+            attemptX.push((Math.floor(Math.random() * 6)));
         }
-        playersInGame.push(playerX);
+        playersInGame.push(attemptX);
     }
 }
 
 //need to add range of secret numbers
 
+numberOfPlayers(10,5);
 
+for (let i = 0; i < playersInGame.length; i++) {
+
+}
